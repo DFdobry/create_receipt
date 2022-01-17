75f8587d88a895f4eac75f43a292b94073cc8066
Fedor <dfdobry@gmail.com>
2022-01-17 21:55:54 +0300

    свойства

---
+++ create_receipt.py
@@ -0,0 +1,7 @@
+properties": {
+    "rqUID": {
+      "description": "Уникальный идентификатор запроса",
+      "type": "string",
+      "maxLength": 32,
+      "pattern": "^(([0-9]|[a-f]|[A-F]){32})$"
+    }
